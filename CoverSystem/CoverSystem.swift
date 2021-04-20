f017868e37c3aa170327dba8ff7427ddb9fdcec3
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2021-04-20 12:03:14 -0400

    Updated more comments showing where code idea came from.

--- CoverSystem/CoverSystem.swift
+++ CoverSystem/CoverSystem.swift
@@ -1,6 +1,13 @@
 //
 //  CoverSystem.swift
 //  CoverSystem
+
+//  An example of the cover system I'm trying out. I did NOT come up with the idea for the class and enum.
+//  It is based off of code from Thomas Ricouard.
+//
+// https://github.com/Dimillian/ACHNBrowserUI/blob/main/ACHNBrowserUI/ACHNBrowserUI/env/UIState.swift
+//
+//
 //
 //  Created by Chris Wu on 4/20/21.
 //
